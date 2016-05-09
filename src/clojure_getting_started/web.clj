9fd48fe71c49b07082e026ed91551e57178d75e9
Joe Kutner <jpkutner@gmail.com>
2016-05-09 15:27:55 -0600

    print all hooks

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -9,6 +9,12 @@
 (defn add-shutdown-hook []
   (.addShutdownHook (Runtime/getRuntime) (Thread. (fn [] (println "Shutting down...")))))
 
+(defn list-shutdown-hooks []
+  (let [clazz (Class/forName "java.lang.ApplicationShutdownHooks")
+        field (.getDeclaredField clazz "hooks")]
+      (.setAccessible field true)
+      (println (.get field nil))))
+
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
@@ -22,6 +28,7 @@
 
 (defn -main [& [port]]
   (add-shutdown-hook)
+  (list-shutdown-hooks)
   (let [port (Integer. (or port (env :port) 5000))]
     (jetty/run-jetty (site #'app) {:port port :join? false})))
 
